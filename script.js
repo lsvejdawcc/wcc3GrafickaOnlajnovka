cdf1c35b25ec420fcef20160a30018790366304a
Libor Svejda <libor.svejda@unicorncollege.cz>
2021-12-08 18:00:58 +0100

    cara

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let cnv,ctx;
+
 function poNacteni() {
   document.addEventListener("keydown", stiskKlavesyDolu);
+
+  cnv = document.getElementById("platno");
+  ctx = cnv.getContext("2d");
+
+  kresleni();
+}
+
+function kresleni() {
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = "red";
+  ctx.moveTo(0,0);
+  ctx.lineTo(cnv.width, cnv.height);
+  ctx.stroke();
+
 }
 
 function stiskKlavesyDolu(udalost) {

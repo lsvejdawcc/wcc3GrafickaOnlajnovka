bf7150448bfdfcfaeb33b39c5da93470cd07a02e
Libor Svejda <libor.svejda@unicorncollege.cz>
2021-12-08 18:12:05 +0100

    pohyb kruhu

--- script.js
+++ script.js
@@ -6,16 +6,35 @@ function poNacteni() {
   cnv = document.getElementById("platno");
   ctx = cnv.getContext("2d");
 
-  kresleni();
+  setInterval(kresleni, 100);
 }
 
+let kruhX = 300;
+let kruhY = 80;
+let kruhRychlostX = 4;
+
 function kresleni() {
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
+
   ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.strokeStyle = "red";
   ctx.moveTo(0,0);
   ctx.lineTo(cnv.width, cnv.height);
   ctx.stroke();
+
+  //plny obdelnik
+  ctx.beginPath();
+  ctx.fillStyle = "green";
+  ctx.rect(10, 200, 100, 60);
+  ctx.fill();
+
+  //kruh
+  kruhX = kruhX + kruhRychlostX;
+  ctx.beginPath();
+  ctx.fillStyle = "blue";
+  ctx.arc(kruhX, kruhY, 50, 0, 2*Math.PI);
+  ctx.fill();
 
 }
 
